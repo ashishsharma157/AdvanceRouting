a92857e96768bbfcd0736729911406f627a06720
ashishsharma157 <sharma.ashish78@gmail.com>
2024-06-24 17:48:29 -0700

    form submit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,8 +24,8 @@ import HomePage from './pages/Home';
 import ErrorPage from './pages/ErrorPage';
 import RootLayout from './pages/Root';
 import EventPage,{loader as eventsLoader} from './pages/Events';
-import EventDetailPage from './pages/EventDetail';
-import NewEventPage from './pages/NewEvent';
+import EventDetailPage,{loader as eventDetailLoader, action as deleteEventAction} from './pages/EventDetail';
+import NewEventPage, {action as newEventAction} from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
 import EventsRootLayout from "./pages/EventsRoot";
 
@@ -39,9 +39,20 @@ var route=createBrowserRouter([
         element:<EventsRootLayout/>,
         children:[
           {path:'', element:<EventPage/>, loader: eventsLoader},
-          {path:':eventId', element:<EventDetailPage/>},
-          {path:'new', element:<NewEventPage/>},
-          {path:':eventId/edit', element:<EditEventPage/>}
+          {
+            path:':eventId',
+            id:'event-detail', 
+            loader:eventDetailLoader,
+            children:[
+              {
+                index:true, element:<EventDetailPage/>, 
+                action:deleteEventAction,
+              },
+              {path:'edit', element:<EditEventPage/>}                
+            ]
+          },
+          {path:'new', element:<NewEventPage/>, action:newEventAction},
+ 
         ]
       }
 
